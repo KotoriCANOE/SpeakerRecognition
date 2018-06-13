fdb09cf6af28a05ec85ceb4ad40d02f44234ff16
KotoriCANOE <sanma.kote@gmail.com>
2018-06-13 14:48:06 +0800

    Data: incorporate multi-threading into multi-processing

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
+from pydub import AudioSegment
 import numpy as np
 import os
-from utils import eprint, listdir_files
-from pydub import AudioSegment
 import random
+from utils import eprint, listdir_files
 
 class Data:
     def __init__(self, config):
@@ -11,6 +11,7 @@ class Data:
         self.max_steps = None
         self.batch_size = None
         self.val_size = None
+        self.processes = None
         self.threads = None
         self.prefetch = None
         self.buffer_size = None
@@ -24,7 +25,8 @@ class Data:
     @staticmethod
     def add_arguments(argp):
         # pre-processing parameters
-        argp.add_argument('--threads', type=int, default=16)
+        argp.add_argument('--processes', type=int, default=4)
+        argp.add_argument('--threads', type=int, default=5)
         argp.add_argument('--prefetch', type=int, default=64)
         argp.add_argument('--buffer-size', type=int, default=1024)
 
@@ -55,49 +57,64 @@ class Data:
             .format(len(self.main_set), self.epoch_steps, self.num_epochs, self.max_steps))
 
     @staticmethod
-    def extract_batch(batch_set, num_labels):
+    def process_sample(id_, file, num_labels):
         # parameters
         sample_rate = 16000
         slice_duration = 2000
         slice_samples = slice_duration * sample_rate // 1000
-        #norm_factor = 1 / (1 << 31)
+        # read from file
+        audio = AudioSegment.from_file(file)
+        #channels = audio.channels
+        #sample_bytes = audio.frame_width
+        #sample_rate = audio.frame_rate
+        #duration = int(audio.duration_seconds * 1000)
+        #samples = int(audio.frame_count())
+        # to np.array
+        data = np.array(audio.get_array_of_samples(), copy=False)
+        samples = data.shape[-1]
+        # slice
+        start = random.randint(0, samples - slice_samples)
+        data = data[start : start + slice_samples]
+        # normalization
+        norm_factor = 1 / audio.max
+        data = data.astype(np.float32) * norm_factor
+        # convert to CHW format
+        data = np.expand_dims(np.expand_dims(data, 0), 0)
+        # one-hot label
+        label = np.zeros((num_labels,), np.float32)
+        label[id_] = 1
+        # return
+        return data, label
+
+    def extract_batch(self, batch_set):
+        from concurrent.futures import ThreadPoolExecutor
         # initialize
         inputs = []
         labels = []
         # load all the data
-        for id_, file in batch_set:
-            # read from file
-            audio = AudioSegment.from_file(file)
-            #channels = audio.channels
-            #sample_bytes = audio.frame_width
-            #sample_rate = audio.frame_rate
-            #duration = int(audio.duration_seconds * 1000)
-            #samples = int(audio.frame_count())
-            # to np.array
-            data = np.array(audio.get_array_of_samples(), copy=False)
-            samples = data.shape[-1]
-            # slice
-            start = random.randint(0, samples - slice_samples)
-            data = data[start : start + slice_samples]
-            # normalization
-            norm_factor = 1 / audio.max
-            data = data.astype(np.float32) * norm_factor
-            # convert to CHW format
-            data = np.expand_dims(np.expand_dims(data, 0), 0)
-            # one-hot label
-            label = np.zeros((num_labels,), np.float32)
-            label[id_] = 1
-            # final data
-            inputs.append(data)
-            labels.append(label)
+        if self.threads == 1:
+            for id_, file in batch_set:
+                data, label = Data.process_sample(id_, file, self.out_channels)
+                inputs.append(data)
+                labels.append(label)
+        else:
+            with ThreadPoolExecutor(self.threads) as executor:
+                futures = []
+                for id_, file in batch_set:
+                    futures.append(executor.submit(Data.process_sample, id_, file, self.out_channels))
+                # final data
+                for future in futures:
+                    data, label = future.result()
+                    inputs.append(data)
+                    labels.append(label)
         # stack data to form a batch
         inputs = np.stack(inputs)
         labels = np.stack(labels)
         return inputs, labels
 
     def gen_main(self, start=0):
-        from concurrent.futures import ThreadPoolExecutor
-        with ThreadPoolExecutor(self.threads) as executor:
+        from concurrent.futures import ProcessPoolExecutor
+        with ProcessPoolExecutor(self.processes) as executor:
             futures = []
             # loop over epochs
             for epoch in range(start // self.epoch_steps, self.num_epochs):
@@ -108,7 +125,7 @@ class Data:
                 for step in range(step_start, step_stop):
                     offset = step * self.batch_size
                     batch_set = self.main_set[offset : offset + self.batch_size]
-                    futures.append(executor.submit(self.extract_batch, batch_set, self.out_channels))
+                    futures.append(executor.submit(self.extract_batch, batch_set))
                     # yield the data beyond prefetch range
                     while len(futures) >= self.prefetch:
                         future = futures.pop(0)
@@ -118,4 +135,4 @@ class Data:
                 yield future.result()
 
     def get_val(self):
-        return self.extract_batch(self.val_set, self.out_channels)
+        return self.extract_batch(self.val_set)
